20eed2dfff6f4256ced8a090f025437916fe598c
nithinvenugopal <nithin.sv3@gmail.com>
2017-09-22 10:22:18 -0400

    Final commit

--- pca.py
+++ pca.py
@@ -9,16 +9,35 @@ def calculate(files, scale):
 
 def call(data,scale):
     data = data - data.mean(axis=0)
+    varx = np.var(data[:,0])
+    print("--------Variance of X--------")
+    print(varx)
+    vary = np.var(data[:,1])
+    print("\n --------Variance of Y--------")
+    print(vary)    
+    varz = np.var(data[:,2])
+    print("\n --------Variance of Z--------")
+    print(varz)
+    covxy = np.cov(data[:,0],data[:,1], rowvar=False)
+    print("\n --------Covariance of x and y--------")
+    print(covxy[0,1])
+    covyz = np.cov(data[:,1],data[:,2],rowvar=False)
+    print("\n --------Covariance of y and z--------")
+    print(covyz[0,1])
+    
+    
     covmatrix = np.cov(data, rowvar=False)
     eigvalue, eigvector = LA.eig(covmatrix)
     index = eigvalue.argsort()[::-1]   
     eigvalue = eigvalue[index]
     eigvector = eigvector[:,index]
-    print("--------Eigen Values are--------")
+    print("\n --------Eigen Values are--------")
     print(eigvalue)
-    print("--------Eigen Vectors are--------")
+    print("\n --------Eigen Vectors(Principal Components) are--------")
     print(eigvector)
     selpc = eigvector[:,:scale]
     newx = np.matmul(data,selpc)
-    print("--------Transformed Matrix is--------")
+    print("\n --------Transformed Matrix is--------")
     print(newx)
+    
+calculate('dataset_1.csv',3)
